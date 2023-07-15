d709b80ebea9fa730f771bde4f0774a4a14664eb
DhanaPrakash <dhanaprakash1998@gmail.com>
2023-07-15 13:35:29 +0530

    American development team

---
+++ src/gitCode.java
@@ -0,0 +1,24 @@
+
+public class gitCode {
+	
+	public void Selenium()
+	{
+		System.out.println("Selenium code");
+	}
+	
+	public void Appium()
+	{
+		System.out.println("Appium software");
+	}
+	
+	public void RESTAPI()
+	{
+		System.out.println("Implmenting RESTAPI");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
