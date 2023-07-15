1ce5a5071ee221ed2a9c781c19037ecc3e341040
DhanaPrakash <dhanaprakash1998@gmail.com>
2023-07-15 13:09:10 +0530

    Architect Asian development

--- src/gitDemo.java
+++ src/gitDemo.java
@@ -1,11 +1,23 @@
 
 public class gitDemo {
+	
+	public void gitApps()
+	{
+		System.out.println("New IOS");
+		System.out.println("New Android");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		gitDemo gd = new gitDemo();
+		
 		System.out.println("Git Demonstration");
 		System.out.println("Git Demonstration1");
+		System.out.println("Git Demo");
+		System.out.println("Git Demonstration2");
+		System.out.println("Git Demo1");
+		gd.gitApps();
 
 	}
 
